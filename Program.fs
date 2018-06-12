1e84c23bdf1ddad9427d4f841bd9f3b3245682ee
gfritz <gfritz@cardlytics.com>
2018-06-12 09:12:48 -0400

    commit to show on https://stackoverflow.com/q/50805319/2796907

---
+++ Program.fs
@@ -0,0 +1,21 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Expecto
+
+let rec evaluate (x: string) : char =
+  match x with
+  // ... cases
+  | _ -> failwith "illogical"
+
+[<Tests>]
+let tests = testList "samples" [
+    test "non-logic" {
+        Expect.throws (fun _ -> evaluate "Kirkspeak" |> ignore) "illogical"
+    }
+]
+
+
+[<EntryPoint>]
+let main argv =
+    Tests.runTestsInAssembly defaultConfig argv
